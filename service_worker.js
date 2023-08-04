c6ddd43e03dcf51349afe3b5cbcc568fdab5a377
angeltee123 <45322441+angeltee123@users.noreply.github.com>
2023-08-04 13:47:33 -0500

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,17 +1,7 @@
-// Cached core static resources 
-self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open("static").then(cache=>{
-      return cache.addAll(["./",'./images/manifest-icon-192.maskable']);
-    })
-  );
-});
-
-// Fatch resources
-self.addEventListener("fetch",e=>{
-  e.respondWith(
-    caches.match(e.request).then(response=>{
-      return response||fetch(e.request);
-    })
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+      })
   );
 });
